e3cde52d86a720994eb1b8d00491750cb064bedf
Carlos Blanco Herrero <carlosblancoh@usal.es>
2021-03-18 09:49:25 +0100

    file uploading

--- api/main.py
+++ api/main.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hola de novo!'
+UPLOAD_PATH = '/home/ubuntu/files'
+
+@app.route('/', methods = ['POST'])
+def read_plate():
+
+    if 'file' not in request.plate:
+        return 'No se ha seleccionado un archivo válido.', 400
+    file = request.plate['file']
+
+    if file.filename == ''
+        return 'No se ha seleccionado un archivo válido.', 400
+    content = file.stream.read()
+    return content
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
